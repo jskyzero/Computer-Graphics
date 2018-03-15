5b68cae1ba3067f55b03c854767e4947e5f1affe
jskyzero <jskyzero@outlook.com>
2018-03-15 23:58:42 +0800

    add runable opengl test

--- include/hardway.hpp
+++ include/hardway.hpp
@@ -1,7 +1,12 @@
 #ifndef __HARDWAY_HPP__
 #define __HARDWAY_HPP__
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 #endif
