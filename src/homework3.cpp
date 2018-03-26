d0b9efb2e3f431fe596387be2dea916d6ceae5b9
jskyzero <jskyzero@outlook.com>
2018-03-26 21:28:24 +0800

    add ugly drawable points

--- src/homework3.cpp
+++ src/homework3.cpp
@@ -2,18 +2,20 @@
 
 #include <homework3.hpp>
 #include <opengl_helper.hpp>
+#include <vector>
 
 #include "imgui.h"
 #include "imgui_impl_glfw_gl3.h"
 
-
-GLfloat vertex_array[] = {1.0, 2.0, 1.0, 2.0, 1.0, 2.0};
+std::vector<GLfloat> vertexs {0.0, 0.5, 0.0, -0.5, 0.0, 0.0, 0.5, 0.0, 0.0};
+// GLfloat vertex_array[] = {1.0, 2.0, 1.0, 2.0, 1.0, 2.0};
 
 int main() {
   GLFWwindow* window;
   int width = 800, height = 600;
   bool show_circle = false;
   float point_spacing = 0.01;
+  GLuint point_vao[3], Circle[3];
 
   auto initial_window = [&] {
     window = glfwCreateWindow(width, height, "homework3", NULL, NULL);
@@ -21,12 +23,27 @@ int main() {
     glfwMakeContextCurrent(window);
   };
 
-  helper::initial_opengl(initial_window, window);
+  auto update_point_vao = []() {
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexs.size(), vertexs.data(), GL_STREAM_DRAW);
+  };
 
+  auto set_point_vao = [update_point_vao](GLuint VAO, GLuint VBO,
+                                                        GLuint EBO) {
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexs.size(), vertexs.data(), GL_STREAM_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void*)0);
+    glEnableVertexAttribArray(0);
+    update_point_vao();
+    glPointSize(10);
+  };
+
+  helper::initial_opengl(initial_window, window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   GLuint simple_shader_program = helper::create_program_with_shader(
       "../shader/simple.vs.glsl", "../shader/simple.fs.glsl");
+  helper::set_vao(point_vao[0], point_vao[1], point_vao[2],
+                  set_point_vao);
 
 
   while (!glfwWindowShouldClose(window)) {
@@ -42,19 +59,22 @@ int main() {
 
     ImGui::Checkbox("show circle", &show_circle);
     if (!show_circle) {
-      ImGui::SliderFloat2("Vertex 1", vertex_array, -1, 1);
-      ImGui::SliderFloat2("Vertex 2", vertex_array + 2, -1, 1);
-      ImGui::SliderFloat2("Vertex 3", vertex_array + 4, -1, 1);
+      ImGui::SliderFloat2("Vertex 1", vertexs.data(), -1, 1);
+      ImGui::SliderFloat2("Vertex 2", vertexs.data() + 3, -1, 1);
+      ImGui::SliderFloat2("Vertex 3", vertexs.data() + 6, -1, 1);
     } else {
-      ImGui::SliderFloat2("certer 1", vertex_array, -1, 1);
+      ImGui::SliderFloat2("certer 1", vertexs.data(), -1, 1);
     }
     ImGui::SliderFloat("Point Space", &point_spacing, 0, 1);
 
     ImGui::End();
 
     glfwGetWindowSize(window, &width, &height);
+    glUseProgram(simple_shader_program);
     // glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels); //draw pixel
-
+    glBindVertexArray(point_vao[0]);
+    update_point_vao();
+    glDrawArrays(GL_POINTS, 0, 3);
     ImGui::Render();
     ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
