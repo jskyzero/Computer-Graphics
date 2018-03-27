c9ff9e9c1ff475e620986c72acda9ae008172a1a
jskyzero <jskyzero@outlook.com>
2018-03-27 13:22:53 +0800

    finish 3.1

--- include/homework3.hpp
+++ include/homework3.hpp
@@ -1,14 +1,122 @@
-#ifndef __HOMEWORK2__
-#define __HOMEWORK2__
+#ifndef __HOMEWORK3__
+#define __HOMEWORK3__
 
 #include <glad/glad.h>   // for glad
 #include <GLFW/glfw3.h>  // for glfw
 
+#include <vector>
+#include <cmath>
 
 
-void process_input(GLFWwindow* window);
+void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
+void add_point(std::vector<GLfloat>& v, GLfloat x, GLfloat y) {
+  v.insert(v.end(), x);
+  v.insert(v.end(), y);
+  v.insert(v.end(), 0);
+}
+
+void plot_line(std::vector<GLfloat>& points, GLfloat begin[], GLfloat end[],
+               float point_spacing) {
+  auto plot_line_low = [&](GLfloat begin[], GLfloat end[]) {
+    GLfloat dx = end[0] - begin[0];
+    GLfloat dy = end[1] - begin[1];
+    GLfloat yi = point_spacing;
+
+    if (dy < 0) {
+      yi = -yi;
+      dy = -dy;
+    }
+    GLfloat D = 2 * dy - dx;
+    GLfloat x = begin[0];
+    GLfloat y = begin[1];
+
+    while (x < end[0]) {
+      add_point(points, x, y);
+      if (D > 0) {
+        y = y + yi;
+        D = D - 2 * dx;
+      }
+      D = D + 2 * dy;
+      x = x + point_spacing;
+    }
+  };
+
+  auto plot_line_high = [&](GLfloat begin[], GLfloat end[]) {
+    GLfloat dx = end[0] - begin[0];
+    GLfloat dy = end[1] - begin[1];
+    GLfloat xi = point_spacing;
+
+    if (dx < 0) {
+      xi = -xi;
+      dx = -dx;
+    }
+    GLfloat D = 2 * dx - dy;
+    GLfloat x = begin[0];
+    GLfloat y = begin[1];
+
+    while (y < end[1]) {
+      add_point(points, x, y);
+      if (D > 0) {
+        x = x + xi;
+        D = D - 2 * dy;
+      }
+      D = D + 2 * dx;
+      y = y + point_spacing;
+    }
+  };
+
+  if (std::abs(end[1] - begin[1]) < std::abs(end[0] - begin[0])) {
+    if (begin[0] > end[0]) {
+      plot_line_low(end, begin);
+    } else {
+      plot_line_low(begin, end);
+    }
+  } else {
+    if (begin[1] > end[1]) {
+      plot_line_high(end, begin);
+    } else {
+      plot_line_high(begin, end);
+    }
+  }
+}
+
+  // // initial version use float
+  // auto plot_line = [&](GLfloat begin[], GLfloat end[]) {
+  //   float space = point_spacing;
+
+  //   GLfloat delta_x = begin[0] - end[0];
+  //   GLfloat delta_y = begin[1] - end[1];
+  //   if (delta_x == 0) { // vertical
+
+  //   } else {
+      
+  //     GLfloat delta_err = std::abs(delta_y / delta_x);
+  //     GLfloat error = 0;
+
+  //     GLfloat x = begin[0], y = begin[1];
+  //     GLfloat change_x = begin[0] < end[0] ? space : -space;
+  //     GLfloat change_y = begin[1] < end[1] ? space * delta_err : -space * delta_err;
+  //     // std::cout << change_x << " " << change_y  << std::endl;
+
+  //     for (int i = 0; i < (int)abs(delta_x / space); i++) {
+  //       add_point(points, x , y);
+  //       error = error + delta_err;
+  //       while (error >= 0.5) {
+  //         y += change_y;
+  //         error -= 1.0;
+  //       }
+  //       // y += change_y;
+  //       x += change_x;
+  //     }
+  //   }
+  // };
 
 #endif
--- src/homework3.cpp
+++ src/homework3.cpp
@@ -2,20 +2,23 @@
 
 #include <homework3.hpp>
 #include <opengl_helper.hpp>
-#include <vector>
 
 #include "imgui.h"
 #include "imgui_impl_glfw_gl3.h"
 
-std::vector<GLfloat> vertexs {0.0, 0.5, 0.0, -0.5, 0.0, 0.0, 0.5, 0.0, 0.0};
-// GLfloat vertex_array[] = {1.0, 2.0, 1.0, 2.0, 1.0, 2.0};
-
 int main() {
   GLFWwindow* window;
+  GLuint point_vao[3];
+  std::vector<GLfloat> vertexs{
+      0.0,  0.5, 0.0,  // top
+      -0.5, 0.0, 0.0,  // left
+      0.5,  0.0, 0.0   // down
+  };
+  std::vector<GLfloat> points{};
+
   int width = 800, height = 600;
   bool show_circle = false;
-  float point_spacing = 0.01;
-  GLuint point_vao[3], Circle[3];
+  float point_spacing = 0.001;
 
   auto initial_window = [&] {
     window = glfwCreateWindow(width, height, "homework3", NULL, NULL);
@@ -23,35 +26,19 @@ int main() {
     glfwMakeContextCurrent(window);
   };
 
-  auto update_point_vao = []() {
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexs.size(), vertexs.data(), GL_STREAM_DRAW);
+  auto update_point_vao = [&points]() {
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * points.size(),
+                 points.data(), GL_STREAM_DRAW);
   };
 
-  auto set_point_vao = [update_point_vao](GLuint VAO, GLuint VBO,
-                                                        GLuint EBO) {
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexs.size(), vertexs.data(), GL_STREAM_DRAW);
+  auto set_point_vao = [update_point_vao](GLuint VAO, GLuint VBO, GLuint EBO) {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
     update_point_vao();
-    glPointSize(10);
   };
 
-  helper::initial_opengl(initial_window, window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  GLuint simple_shader_program = helper::create_program_with_shader(
-      "../shader/simple.vs.glsl", "../shader/simple.fs.glsl");
-  helper::set_vao(point_vao[0], point_vao[1], point_vao[2],
-                  set_point_vao);
-
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-    process_input(window);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    ImGui_ImplGlfwGL3_NewFrame();
+  auto create_imgui = [&]() {
     ImGui::Begin("Menu");
     ImGui::Text("Welcome");
     ImGui::Text("Average %.3f ms/frame (%.1f FPS)",
@@ -63,31 +50,47 @@ int main() {
       ImGui::SliderFloat2("Vertex 2", vertexs.data() + 3, -1, 1);
       ImGui::SliderFloat2("Vertex 3", vertexs.data() + 6, -1, 1);
     } else {
-      ImGui::SliderFloat2("certer 1", vertexs.data(), -1, 1);
+      ImGui::SliderFloat2("Certer 1", vertexs.data(), -1, 1);
     }
-    ImGui::SliderFloat("Point Space", &point_spacing, 0, 1);
-
+    ImGui::SliderFloat("Point Space", &point_spacing, 0.001, 0.1);
     ImGui::End();
+  };
+
+  helper::initial_opengl(initial_window, window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  GLuint simple_shader_program = helper::create_program_with_shader(
+      "../shader/simple.vs.glsl", "../shader/simple.fs.glsl");
+  helper::set_vao(point_vao[0], point_vao[1], point_vao[2], set_point_vao);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    process_input(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ImGui_ImplGlfwGL3_NewFrame();
+    create_imgui();
 
     glfwGetWindowSize(window, &width, &height);
-    glUseProgram(simple_shader_program);
-    // glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels); //draw pixel
-    glBindVertexArray(point_vao[0]);
-    update_point_vao();
-    glDrawArrays(GL_POINTS, 0, 3);
+
+    // clear last time points
+    points.clear();
+    if (!show_circle) {
+      glBindVertexArray(point_vao[0]);
+      glUseProgram(simple_shader_program);
+      plot_line(points, vertexs.data(), vertexs.data() + 3, point_spacing);
+      plot_line(points, vertexs.data(), vertexs.data() + 6, point_spacing);
+      plot_line(points, vertexs.data() + 3, vertexs.data() + 6, point_spacing);
+      update_point_vao();
+
+      glPointSize(point_spacing * std::min(width, height));
+      glDrawArrays(GL_POINTS, 0, points.size() / 3);
+    } else {
+    }
     ImGui::Render();
     ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
   }
 
   helper::exit_program();
-}
-
-void process_input(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
 }
