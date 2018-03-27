5246b286c186b3ebb1171adea73a6e9f01197111
jskyzero <jskyzero@outlook.com>
2018-03-27 19:10:47 +0800

    finish homework3

--- include/homework3.hpp
+++ include/homework3.hpp
@@ -4,9 +4,8 @@
 #include <glad/glad.h>   // for glad
 #include <GLFW/glfw3.h>  // for glfw
 
-#include <vector>
 #include <cmath>
-
+#include <vector>
 
 void process_input(GLFWwindow* window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -88,6 +87,83 @@ void plot_line(std::vector<GLfloat>& points, GLfloat begin[], GLfloat end[],
   }
 }
 
+void plot_circle(std::vector<GLfloat>& points, GLfloat center[], float radius,
+                 float point_spacing) {
+  GLfloat x = radius - point_spacing;
+  GLfloat y = 0;
+  GLfloat dx = point_spacing;
+  GLfloat dy = point_spacing;
+  GLfloat err = dx - (radius * 2);
+
+  while (x >= y) {
+    add_point(points, center[0] + x, center[1] + y);
+    add_point(points, center[0] + y, center[1] + x);
+    add_point(points, center[0] - y, center[1] + x);
+    add_point(points, center[0] - x, center[1] + y);
+    add_point(points, center[0] - x, center[1] - y);
+    add_point(points, center[0] - y, center[1] - x);
+    add_point(points, center[0] + y, center[1] - x);
+    add_point(points, center[0] + x, center[1] - y);
+
+    if (err <= 0) {
+      y += point_spacing;
+      err += dy;
+      dy += 2 * point_spacing;
+    }
+    if (err > 0) {
+      x -= point_spacing;
+      dx += 2 * point_spacing;
+      err += dx - (radius * 2);
+    }
+  }
+}
+
+void fill_triangle(std::vector<GLfloat>& points, GLfloat A[], GLfloat B[],
+                   GLfloat C[], float point_spacing) {
+  auto sign = [](GLfloat A[], GLfloat B[], GLfloat C[]) {
+    return (A[0] - C[0]) * (B[1] - C[1]) - (B[0] - C[0]) * (A[1] - C[1]);
+  };
+
+  auto in_triangle = [sign](GLfloat x, GLfloat y, GLfloat A[], GLfloat B[],
+                            GLfloat C[]) {
+    GLfloat point[]{x, y};
+    bool b1 = sign(point, A, B) < 0.0f;
+    bool b2 = sign(point, B, C) < 0.0f;
+    bool b3 = sign(point, C, A) < 0.0f;
+    return (b1 == b2) && (b2 == b3);
+  };
+
+  GLfloat x_min = std::min(std::min(A[0], B[0]), C[0]);
+  GLfloat y_min = std::min(std::min(A[1], B[1]), C[1]);
+  GLfloat x_max = std::max(std::max(A[0], B[0]), C[0]);
+  GLfloat y_max = std::max(std::max(A[1], B[1]), C[1]);
+
+  for (GLfloat x = x_min; x < x_max; x += point_spacing) {
+    for (GLfloat y = y_min; y < y_max; y += point_spacing) {
+      if (in_triangle(x, y, A, B, C)) add_point(points, x, y);
+    }
+  }
+}
+
+void fill_circle(std::vector<GLfloat>& points, GLfloat center[], float radius,
+                 float point_spacing) {
+  auto in_circle = [](GLfloat x, GLfloat y, GLfloat center[], float radius) {
+    return 0 > ((x - center[0]) * (x - center[0]) +
+                (y - center[1]) * (y - center[1]) - radius * radius);
+  };
+
+  GLfloat x_min = center[0] - radius;
+  GLfloat y_min = center[1] - radius;
+  GLfloat x_max = center[0] + radius;
+  GLfloat y_max = center[1] + radius;
+
+  for (GLfloat x = x_min; x < x_max; x += point_spacing) {
+    for (GLfloat y = y_min; y < y_max; y += point_spacing) {
+      if (in_circle(x, y, center, radius)) add_point(points, x, y);
+    }
+  }
+}
+
   // // initial version use float
   // auto plot_line = [&](GLfloat begin[], GLfloat end[]) {
   //   float space = point_spacing;
@@ -97,13 +173,14 @@ void plot_line(std::vector<GLfloat>& points, GLfloat begin[], GLfloat end[],
   //   if (delta_x == 0) { // vertical
 
   //   } else {
-      
+
   //     GLfloat delta_err = std::abs(delta_y / delta_x);
   //     GLfloat error = 0;
 
   //     GLfloat x = begin[0], y = begin[1];
   //     GLfloat change_x = begin[0] < end[0] ? space : -space;
-  //     GLfloat change_y = begin[1] < end[1] ? space * delta_err : -space * delta_err;
+  //     GLfloat change_y = begin[1] < end[1] ? space * delta_err : -space *
+  //     delta_err;
   //     // std::cout << change_x << " " << change_y  << std::endl;
 
   //     for (int i = 0; i < (int)abs(delta_x / space); i++) {
--- src/homework3.cpp
+++ src/homework3.cpp
@@ -17,8 +17,8 @@ int main() {
   std::vector<GLfloat> points{};
 
   int width = 800, height = 600;
-  bool show_circle = false;
-  float point_spacing = 0.001;
+  bool show_circle = false, fill = false;
+  float point_spacing = 0.001, radius = 0.5;
 
   auto initial_window = [&] {
     window = glfwCreateWindow(width, height, "homework3", NULL, NULL);
@@ -45,12 +45,14 @@ int main() {
                 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
     ImGui::Checkbox("show circle", &show_circle);
+    ImGui::Checkbox("fill color", &fill);
     if (!show_circle) {
       ImGui::SliderFloat2("Vertex 1", vertexs.data(), -1, 1);
       ImGui::SliderFloat2("Vertex 2", vertexs.data() + 3, -1, 1);
       ImGui::SliderFloat2("Vertex 3", vertexs.data() + 6, -1, 1);
     } else {
       ImGui::SliderFloat2("Certer 1", vertexs.data(), -1, 1);
+      ImGui::SliderFloat("Radius", &radius, 0.001, 1);
     }
     ImGui::SliderFloat("Point Space", &point_spacing, 0.001, 0.1);
     ImGui::End();
@@ -75,18 +77,25 @@ int main() {
 
     // clear last time points
     points.clear();
+    glBindVertexArray(point_vao[0]);
+    glUseProgram(simple_shader_program);
     if (!show_circle) {
-      glBindVertexArray(point_vao[0]);
-      glUseProgram(simple_shader_program);
-      plot_line(points, vertexs.data(), vertexs.data() + 3, point_spacing);
-      plot_line(points, vertexs.data(), vertexs.data() + 6, point_spacing);
-      plot_line(points, vertexs.data() + 3, vertexs.data() + 6, point_spacing);
-      update_point_vao();
-
-      glPointSize(point_spacing * std::min(width, height));
-      glDrawArrays(GL_POINTS, 0, points.size() / 3);
+      if (fill) fill_triangle(points, vertexs.data(), vertexs.data() + 3, vertexs.data() + 6, point_spacing);
+      else {
+        plot_line(points, vertexs.data(), vertexs.data() + 3, point_spacing);
+        plot_line(points, vertexs.data(), vertexs.data() + 6, point_spacing);
+        plot_line(points, vertexs.data() + 3, vertexs.data() + 6, point_spacing);
+      }
+      
     } else {
+      if (fill) fill_circle(points, vertexs.data(), radius, point_spacing);
+      else plot_circle(points, vertexs.data(), radius, point_spacing);
     }
+    update_point_vao();
+
+    glPointSize(point_spacing * std::min(width, height));
+    glDrawArrays(GL_POINTS, 0, points.size() / 3);
+
     ImGui::Render();
     ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
