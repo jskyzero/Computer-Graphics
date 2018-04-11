084cda55f53f21e60dbe816b73c5f61a8a761dab
jskyzero <jskyzero@outlook.com>
2018-04-11 22:28:17 +0800

    seems finish

--- include/homework5.hpp
+++ include/homework5.hpp
@@ -8,13 +8,9 @@
 #include <vector>
 #include <algorithm>
 
-void process_input(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
 
 #endif
--- src/homework5.cpp
+++ src/homework5.cpp
@@ -1,6 +1,7 @@
 #include <iostream>  // basic input output
 
 #include <homework5.hpp>      // header file
+#include <camera.h>
 #include <opengl_helper.hpp>  // helper library
 // glm library
 #include <glm/glm.hpp>
@@ -11,6 +12,19 @@
 #include "imgui_impl_glfw_gl3.h"
 
 constexpr glm::vec3 initial_position_k = glm::vec3(-1.5f, 0.5f, -1.5f);
+constexpr unsigned int SCR_WIDTH = 800;
+constexpr unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 
 int main() {
   // the GLFW window
@@ -18,7 +32,7 @@ int main() {
   // save the vao vbo eao
   GLuint point_vao[3];
   // store window size
-  int width = 800, height = 600;
+  int width = SCR_WIDTH, height = SCR_HEIGHT;
 
   // basic vertices
   std::vector<GLfloat> vertices{
@@ -52,6 +66,8 @@ int main() {
     helper::assert_true(window != NULL, "Failed to create GLFW windows");
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
   };
   // if we need update vao's vbo / eao
   auto update_point_vao = []() {};
@@ -72,7 +88,7 @@ int main() {
   };
   // ImGui input values
   bool orthographic_projection = false, perspective_projection = false,
-       view_changing = false;
+       view_changing = false, input_move = true;
   // 多组(left, right, bottom, top, near, far)
   float orth_left = -2.0f, orth_right = 2.0f, orth_bottom = -2.0f,
         orth_top = 2.0f, orth_near = 0.1f, orth_far = 10.0f, pers_fov = 45.0,
@@ -102,6 +118,7 @@ int main() {
       ImGui::SliderFloat("pers_far", &pers_far, 1.0f, 200.0f);
     }
     ImGui::Checkbox("view changing", &view_changing);
+    ImGui::Checkbox("input move", &input_move);
     ImGui::End();
   };
 
@@ -134,7 +151,10 @@ int main() {
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
-    process_input(window);
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    processInput(window);
     glfwGetWindowSize(window, &width, &height);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     // also clear the depth buffer now!
@@ -168,12 +188,19 @@ int main() {
     }
     // if set view
     if (view_changing) {
-      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::mat4(1.0f);
       float radius = 10.0f;
       float camX = sin(glfwGetTime()) * radius;
       float camZ = cos(glfwGetTime()) * radius;
       view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0),
                          glm::vec3(0.0, 1.0, 0.0));
+    }
+    if (input_move) {
+      model = glm::mat4(1.0f);
+      // pass projection matrix to shader (note that in this case it could change every frame)
+      projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+      // camera/view transformation
+      view = camera.GetViewMatrix();
     }
 
     // pass transformation matrices to the shader
@@ -192,4 +219,59 @@ int main() {
   }
 
   helper::exit_program();
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  std::cout << yoffset << std::endl;
+  camera.ProcessMouseScroll(yoffset);
 }
